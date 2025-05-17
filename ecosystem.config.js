a8d0a95d0d0d04ae543a79e846eda09858f598e6
Timur <tima.gizatullin.01@mail.ru>
2025-05-17 14:16:17 +0500

    Теперь процесс pm2 поднимается на 4000 порту

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
     {
       name: 'your-tehcomf-client',
       script: 'node_modules/next/dist/bin/next',
+      args: '-p 4000',
       exec_mode: 'cluster',
       instances: 'max'
     }
